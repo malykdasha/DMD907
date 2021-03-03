565f85d27d0b9bfadcc74e41b7a547395857be37
ovsy2 <asdfdsdsdsdxd@mail.ru>
2021-03-03 19:56:44 +0300

    Добавил кнопку (задел для выхода в меню)

--- Scripts/GameOver.py
+++ Scripts/GameOver.py
@@ -1,4 +1,6 @@
 from Scripts.Script import ScriptClass
+from Objects.Button import ButtonClass
+#from Game import GameClass
 import pygame
 
 
@@ -8,8 +10,12 @@ class GameOverClass(ScriptClass):
         self.sound_of_death = pygame.mixer.Sound('Sources/lazha.wav')
         self.font = pygame.font.Font(None, 72)
         self.text_surface = self.font.render('GAME OVER!', True, (255, 0, 0))
+        self.mainmenu = ButtonClass(1, 360, 100, 20, 'sth', game)
 
     def run(self):
+        self.mainmenu.draw()
+        self.mainmenu.is_pressed(None)
         self.game.screen.blit(self.text_surface, (20, 160))
         self.sound_of_death.play()
         pygame.mixer.music.pause()
+        pygame.display.flip()
