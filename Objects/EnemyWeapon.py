d575556e5133c70d271bd011487569f778d59a3c
malykdasha <malyk.dascha@mail.ru>
2021-06-30 13:03:24 +0300

    EnemyWeapon check touch

--- Objects/EnemyWeapon.py
+++ Objects/EnemyWeapon.py
@@ -1,4 +1,5 @@
 import Objects
+import Scripts
 import pygame
 
 
@@ -12,6 +13,8 @@ class EnemyWeapon(Objects.ObjectClass):
         self.height = 50
         self.img = self.game.bullet_img
         self.img = pygame.transform.scale(self.img, (self.width, self.height))
+        self.health = 1
+        self.scripts.append(Scripts.CheckTouchEnemy(self, game))
 
     def draw(self):
         self.game.screen.blit(self.img, (self.x, self.y))
